1ee17f1379bcde060e46866efc6bc468c78ec5b8
Alexander Pronin <pronin.alx@gmail.com>
2016-04-09 12:43:43 +0300

    Final commit

--- NSStringAndNSData_Demo.playground/Contents.swift
+++ NSStringAndNSData_Demo.playground/Contents.swift
@@ -1,5 +1,29 @@
-//: Playground - noun: a place where people can play
-
 import UIKit
 
-var str = "Hello, playground"
+let fileManager = NSFileManager.defaultManager()
+
+do {
+    var documents = try fileManager.URLForDirectory(.DocumentDirectory, inDomain: .UserDomainMask, appropriateForURL: nil, create: false)
+    var url = NSURL(string: "quote.txt", relativeToURL: documents)
+    let quote = "Four score and sever years ago."
+    if let url = url {
+        try quote.writeToURL(url, atomically: true, encoding: NSUTF8StringEncoding)
+        var text = try String(contentsOfURL: url)
+    }
+} catch {
+    print("error: \(error)")
+}
+
+if let bee = UIImage(named: "bee.jpg") {
+    let imageData = UIImagePNGRepresentation(bee)
+    do {
+        var documents = try fileManager.URLForDirectory(.DocumentDirectory, inDomain: .UserDomainMask, appropriateForURL: nil, create: false)
+        var url = NSURL(string: "bee.bin", relativeToURL: documents)
+        imageData?.writeToURL(url!, atomically: true)
+        if let beeData = NSData(contentsOfURL: url!) {
+            let newImage = UIImage(data: beeData)!
+        }
+    } catch {
+        print("error: \(error)")
+    }
+}
